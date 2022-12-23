75f2df99537595d6b41390bedfd7ad865e495f79
sandeep4741 <sandeepg@brightcomgroup.com>
2022-12-23 13:01:26 +0000

    third

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -8,6 +8,6 @@ if a > 5:
     print("Last digit of", number, "is", a, "and is greater than 5")
 elif a == 0:
     print("Last digit of", number, "is", a, "and is 0")
-elif a < 6 and b != 0:
+elif a < 6 and a != 0:
     print("Last digit of", number, "is", a, "and is less than 6 and not 0")
 
