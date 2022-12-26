2f11e5c86f8e40331f9179e3959c91d8dfc16b5b
sandeep4741 <sandeepg@brightcomgroup.com>
2022-12-26 04:47:56 +0000

    fourth

--- 2-positive_or_negative.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-number=int(input())
-if number>0:
-    print("is positive")
-elif number==0:
-    print("is zero")
-elif number<0:
-    print("is negative")
---
+++ python-hello_world/4-print_hexa.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+n= 98
+for i in range(n+1):
+    print (i, "=", hex(i))
